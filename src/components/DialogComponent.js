c9c8dc37ae50979acdc8cc2185332f33d78ae609
nicol <nsanchezuceta@gmail.com>
2022-04-07 11:54:20 -0400

    dialog componente agregado

---
+++ src/components/DialogComponent.js
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+import SimpleDialog from "./SimpleDialog";
+
+
+function DialogComponent() {
+
+    const [open, setOpen] = useState(false)
+    const [selectedValue, setSelectedValue] = useState('')
+
+
+    const handleClickOpen = () => {
+        setOpen(true)
+    }
+
+
+    const handleClose = (value) => {
+        setOpen(false)
+        setSelectedValue(value)
+    }
+
+    return (
+
+        <>
+
+
+            <h3>You Selected: {selectedValue}</h3>
+
+            <button variant='outlined' color='primary' onClick={handleClickOpen}>
+                Open the Dart Dialog!
+            </button>
+
+            <SimpleDialog selectedValue={selectedValue} open={open} onClose={handleClose}/>
+
+
+
+        </>
+    );
+}
+
+
+export default DialogComponent;
---
+++ src/components/SimpleDialog.js
@@ -0,0 +1,60 @@
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import React from "react";
+import PersonIcon from '@material-ui/icons/Person';
+import {List} from "@material-ui/core";
+
+
+function SimpleDialog(props) {
+
+    const {onClose, selectedValue, open} = props
+
+
+    const menu = ['helado', 'pizza', 'hot dog', 'cheese burger'];
+
+    const handleClose = () => {
+
+        alert(selectedValue)
+        onClose(selectedValue)
+    }
+
+    const handleListItemClick = (value) => {
+
+        onClose(value)
+    }
+
+    return (
+        <>
+            {/*En el parametro open se le indica al dialog si esta abierto o cerrado
+            en onClose se indica la funcion encargada de cerrar el dialog*/}
+            <Dialog onClose={handleClose} open={open}>
+
+
+                <DialogTitle>Nicol Title?</DialogTitle>
+
+                <List>
+
+                    {menu.map(menu =>(
+                        <ListItem button onClick={() => handleListItemClick(menu)}>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <PersonIcon/>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary={menu}/>
+                        </ListItem>
+                    ))}
+
+
+                </List>
+            </Dialog>
+        </>
+    );
+
+}
+
+export default SimpleDialog;
