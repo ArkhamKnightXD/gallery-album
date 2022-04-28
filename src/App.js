c4e8f2b5a663bad4ecca45b70dbec194744f33ce
nicol <nsanchezuceta@gmail.com>
2022-04-28 16:04:45 -0400

    proyecto terminado.

--- src/App.js
+++ src/App.js
@@ -1,69 +1,111 @@
 import './App.css';
+import './index.css';
+import {motion, AnimatePresence} from 'framer-motion';
+import {useState} from 'react';
 import DialogComponent from "./components/DialogComponent";
 import Gallery from "./components/Gallery";
 import TableForm from "./components/tables/TableForm";
 import axios from "axios";
-import {useEffect, useState} from "react";
+
 import DialogForm from "./components/DialogForm";
+import {Button} from "@material-ui/core";
 
 
 
 function App() {
 
-    const [videojuegos, setVideojuegos] = useState([]);
-    const [videojuegoActual, setVideojuegoActual] = useState(null);
-    const [isOpen, setIsOpen] = useState(false);
+//     const [videojuegos, setVideojuegos] = useState([]);
+//     const [videojuegoActual, setVideojuegoActual] = useState(null);
+//     const [isOpen, setIsOpen] = useState(false);
+//
+//     function obtenerJuegos() {
+// // get es para obtener elementos de la api
+//         axios.get(`http://10.0.0.128:88/api/v1/video-games`).then(response => {
+//
+//             setVideojuegos(response.data);
+//
+//         });
+//     }
+//
+//
+//     const handleClickOpen = () => {
+//
+//         setIsOpen(true);
+//     }
+//
+//
+//
+//     useEffect(() => {
+//
+//         obtenerJuegos();
+//
+//
+//     }, []);
 
-    function obtenerJuegos() {
-// get es para obtener elementos de la api
-        axios.get(`http://10.0.0.128:88/api/v1/video-games`).then(response => {
-
-            setVideojuegos(response.data);
-
-        });
-    }
-
-
-    const handleClickOpen = () => {
-
-        setIsOpen(true);
-    }
+//}
 
 
 
-    useEffect(() => {
 
-        obtenerJuegos();
+const [isOpen, setIsOpen] = useState([false])
 
 
-    }, []);
+
 
 
     return (
 
 //Lo ideal es no tener mucho codigo en app. Lo que se debe de hacer en app es llamar los componentes aqui
-        <div className="App">
-            {/*<Gallery/>*/}
 
+        <div className="App">
+
+            {/*<Gallery/>*/}
 
             {/*<DialogComponent/>*/}
 
-
             {/*<TableSorting/>*/}
 
-
             {/*<ContactForm/>*/}
-<br/>
-            <DialogForm setData={setVideojuegos} videojuegoActual={videojuegoActual} isOpen={isOpen} setIsOpen={setIsOpen}
-            handleClickOpen={handleClickOpen}/>
 
 
-            <TableForm rows={videojuegos} setVideojuegos={setVideojuegos} setVideojuegoActual={setVideojuegoActual}
-                       handleClickOpen={handleClickOpen}/>
+                   {/*game-form*/}
 
 
-        </div>
-    );
+                   {/*<br/>*/}
+{/*            <DialogForm setData={setVideojuegos} videojuegoActual={videojuegoActual} isOpen={isOpen} setIsOpen={setIsOpen}*/}
+{/*            handleClickOpen={handleClickOpen}/>*/}
+
+
+{/*            <TableForm rows={videojuegos} setVideojuegos={setVideojuegos} setVideojuegoActual={setVideojuegoActual}*/}
+{/*                       handleClickOpen={handleClickOpen}/>*/}
+
+
+
+<motion.div transition={{layout: {duration: 1, type: "spring"}}} layout onClick={() => setIsOpen(!isOpen)}
+            className="card" style={{borderRadius: "1rem", boxShadow: "0px 10px 30px rgba(0,0,0,0.5)"}} >
+
+<motion.h2 layout="position">Framer Motion 🚀</motion.h2>
+    {isOpen && (
+    <motion.div>
+        <p initial={{opacity: 0}}
+           animate={{opacity: 1}}
+           transition={{duration: 1}}
+           exit={{opacity: 0}} className="expand">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum justo sed tempus aliquet.
+            Praesent luctus eleifend finibus. Pellentesque tincidunt.</p>
+        <p>turpis nunc eget lorem dolor sed viverra ipsum nunc aliquet</p>
+        <Button className="button01">hide</Button>
+    </motion.div>
+    )}
+
+</motion.div>
+
+
+
+
+
+       </div>
+
+        );
 }
 
 export default App;
