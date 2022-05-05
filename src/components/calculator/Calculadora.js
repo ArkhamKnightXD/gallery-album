2249c4e6364046fdc33a48a7bc4af80c024a5abd
nicol <nsanchezuceta@gmail.com>
2022-05-05 13:53:58 -0400

    calculadora terminada

--- src/components/calculator/Calculadora.js
+++ src/components/calculator/Calculadora.js
@@ -1,5 +1,4 @@
 import {useState} from 'react';
-import {ruRU} from "@material-ui/core/locale";
 import "../../index.css";
 
 
@@ -7,19 +6,42 @@ export default function Calculadora() {
 
     const [calc, setCalc] = useState("");
     const [result, setResult] = useState("");
+    const operations = ['/', '*', '+', '-', '.'];
 
     const createDigits = () => {
         const digits = [];
 
         for (let i = 1; i < 10; i++) {
             digits.push(
-                <button key={i} onClick={() => updateCalc(i)} key={i}>{i}
-                </button>
+                <button key={i} onClick={() => updateCalc(i)} key={i}>{i}</button>
             )
         }
         return digits;
     }
 
+
+    const updateCalc = value => {
+
+
+        //La parte uno de la operacion verifica que si la pantalla esta en blanco y que la operacion sea una de las cuatro
+        //basicas: /*-+, la segunda parte es para evitar que se pueda colocar dos signos seguidos, ej: ++,--
+        if (operations.includes(value) && calc === '' || operations.includes(value) && operations.includes(calc.slice(-1)))
+        {
+            return;
+        }
+          //Aqui no se estan sumando los valores sino se esta juntando el texto.
+        setCalc(calc + value);
+
+
+        if (!operations.includes(value)) {
+            setResult(eval(calc + value).toString());
+        }
+    }
+
+
+
+
+    //Aqui se calcula los valores mediante eval y al final lo volvemos un string con el metodo .toString
     const calculate = () => {
         setCalc(eval(calc).toString());
     }
@@ -29,6 +51,7 @@ export default function Calculadora() {
             return;
         }
 
+        //Con .slice estoy quitando del arreglo el ultimo elemento
         const value = calc.slice(0, -1);
 
         setCalc(value);
@@ -37,25 +60,7 @@ export default function Calculadora() {
 
 
 
-    const operations = ['/', '*', '+', '-', '.'];
 
-    const updateCalc = value => {
-
-        console.log(value);
-        if (
-            operations.includes(value) && calc === '' || operations.includes(value) && operations.includes(calc.slice(-1))
-        )
-        {
-            return;
-        }
-
-        setCalc(calc + value);
-
-
-        if (!operations.includes(value)) {
-            setResult(eval(calc + value).toString());
-        }
-    }
 
 
     return (
