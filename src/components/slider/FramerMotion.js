bd44381c4829fbe2e93dc0729d827ac2b5150262
nicol <nsanchezuceta@gmail.com>
2022-05-05 09:30:16 -0400

    comentarios.

--- src/components/slider/FramerMotion.js
+++ src/components/slider/FramerMotion.js
@@ -6,10 +6,12 @@ import {useRef, useEffect, useState} from 'react';
 export default function FramerMotion() {
 
     const [width, setWidth] = useState(0);
+    //Mediante useRef obtenemos la referencia de la pagina en que estamos
     const carousel = useRef();
 
 
     useEffect(() => {
+
         setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
     }, []);
 
@@ -18,8 +20,14 @@ export default function FramerMotion() {
 
         <>
 
+            {/*En el motion initial se le indica la referencia de la pagina y la forma que tendra el cursor*/}
             <motion.div ref={carousel} className="carousel" whileTap={{cursor: "grabbing"}}>
+
+                {/*Aqui en drag se indica la direccion en que se moveran las imagenes, si es x sera de izquierda a derecha.
+                y si es y sera de arriba a abajo. En dragconstraints se indica el rango de movimiento del slider.*/}
                 <motion.div drag="x" dragConstraints={{right: 0, left: -width}} className="inner-carousel">
+
+                    {/*Aqui se muestra el nuevo arreglo*/}
                     {Images.map((image) => {
                         return (
                             <motion.div className="item" key={image}>
